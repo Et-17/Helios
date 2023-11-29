0db4964c23b299ccba5576eb95100b5f878ea409
Luna Kilborn <leland.kilborn@hotmail.com>
2023-11-28 23:21:29 -0500

    Sort company columns

--- src/CompanyList/CompanyList.vue
+++ src/CompanyList/CompanyList.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { companies, shown_columns, frontend_column_names } from '../companyManagement';
+
+// const sorted_shown_columns = computed(shown_columns.value.sort);
+
+function sort<T>(to_sort: T[]) {
+  return [...to_sort].sort(fancy_sort);
+}
+
+// It's really weird for the company name not to be first and the description
+// not to be last, so this handles that.
+function fancy_sort<T>(a: T, b: T): number {
+  if (a == "name" && b != "name") {
+    return -1;
+  } else if (a != "name" && b == "name") {
+    return 1;
+  } /*nothing is name*/ else if (a == "description" && b != "description") {
+    return 1;
+  } else if (a != "description" && b == "description") {
+    return -1;
+  } /*nothing is desc*/ else if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
 </script>
 
 <template>
   <table class="company-list">
     <thead>
       <tr>
-        <th v-for="column in shown_columns">{{ frontend_column_names[column] }}</th>
+        <th v-for="column in sort(shown_columns)">{{ frontend_column_names[column] }}</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="company in companies">
-        <td v-for="column in shown_columns">{{ company[column] }}</td>
+        <td v-for="column in sort(shown_columns)">{{ company[column] }}</td>
       </tr>
     </tbody>
   </table>
